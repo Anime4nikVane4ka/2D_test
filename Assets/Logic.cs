1edc150d4a6be1849196ea45048ba240c5b75746
Anime4nik_Vane4ka <pisarevim2015@gmail.com>
2026-03-23 19:32:01 +0300

    clouds

--- Assets/Logic.cs
+++ Assets/Logic.cs
@@ -7,6 +7,7 @@ public class Logic : MonoBehaviour
     private int score = 0;
     public Text scoreText;
     public GameObject gameOverScreen;
+    private bool gameOver = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,13 +23,17 @@ public class Logic : MonoBehaviour
     [ContextMenu("Score+")]
     public void add_score(int adder = 1)
     {
-        score += adder;
-        scoreText.text = score.ToString();
+        if (!gameOver)
+        {
+            score += adder;
+            scoreText.text = score.ToString();
+        }        
     }
 
     public void game_over()
     {
         gameOverScreen.SetActive(true);
+        gameOver = true;
     }
 
     public void restart_game()
